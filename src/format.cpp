3a993759b5222101fa71dbfdfdaaef7ac4e3795d
Arya Akmal <60330179+aryaakmal@users.noreply.github.com>
2024-09-21 17:53:59 -0400

    Update format.cpp to return HH:MM:SS

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,33 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times) { 
+   
+ int hours, minutes, seconds;
+ string hrs, mins, secs;
+
+ hours = times/3600;
+ minutes = (times % 3600)/60;
+ seconds = ((times % 3600) % 60);
+
+ if( seconds < 10 ) {
+  secs = "0"+ std::to_string(seconds);
+ }else{
+  secs = std::to_string(seconds);
+ }
+ if( minutes < 10 ) {
+  mins = "0"+ std::to_string(minutes);
+ }else{
+  mins = std::to_string(minutes);
+ }
+ if( hours < 10 ) {
+   hrs = "0" + std::to_string(hours);
+ }else{
+  hrs = std::to_string(hours);
+ }
+
+ return (hrs + ":" + mins + ":" + secs);
+}
