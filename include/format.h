f431c89b656c65eb102e16bd3a8c977e423b6d64
Arya Akmal <60330179+aryaakmal@users.noreply.github.com>
2024-09-26 09:25:28 -0400

    Update format.h

--- include/format.h
+++ include/format.h
@@ -1,10 +1,36 @@
-#ifndef FORMAT_H
-#define FORMAT_H
-
 #include <string>
 
-namespace Format {
-std::string ElapsedTime(long times);  // TODO: See src/format.cpp
-};                                    // namespace Format
+#include "format.h"
 
-#endif
+using std::string;
+
+// DONE: Complete this helper function
+// INPUT: Long int measuring seconds
+// OUTPUT: HH:MM:SS
+string Format::ElapsedTime(long times) { 
+   
+ int hours, minutes, seconds;
+ string hrs, mins, secs;
+
+ hours = times/3600;
+ minutes = (times % 3600)/60;
+ seconds = ((times % 3600) % 60);
+
+ if( seconds < 10 ) {
+  secs = "0"+ std::to_string(seconds);
+ }else{
+  secs = std::to_string(seconds);
+ }
+ if( minutes < 10 ) {
+  mins = "0"+ std::to_string(minutes);
+ }else{
+  mins = std::to_string(minutes);
+ }
+ if( hours < 10 ) {
+   hrs = "0" + std::to_string(hours);
+ }else{
+  hrs = std::to_string(hours);
+ }
+
+ return (hrs + ":" + mins + ":" + secs);
+}
