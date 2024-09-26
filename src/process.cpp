078db60099818b5202fb2a225605cc23567c23ef
Arya Akmal <60330179+aryaakmal@users.noreply.github.com>
2024-09-26 09:39:37 -0400

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,53 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Constructor for Process Class
+Process::Process(int pid) : pid_(pid) {};
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// DONE: Return this process's ID
+int Process::Pid() { return pid_; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// DONE: Return this process's CPU utilization
+float Process::CpuUtilization() { 
+  float active_time = float(LinuxParser::ActiveJiffies(pid_));  //note: seconds not jiffies.
+  float total_time = float(LinuxParser::UpTime() - LinuxParser::UpTime(pid_));
+  cpuUtil_ = active_time/total_time;
+  //return active_time/total_time;
+  return cpuUtil_;
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// DONE: Return the command that generated this process
+string Process::Command() { 
+  return LinuxParser::Command(pid_); 
+}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// DONE: Return this process's memory utilization
+string Process::Ram() { 
+  return LinuxParser::Ram(pid_); 
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// DONE: Return the user (name) that generated this process
+string Process::User() { 
+  int uid = std::stoi(LinuxParser::Uid(pid_));
+  return LinuxParser::User(uid); 
+}
+
+// DONE: Return the age of this process (in seconds)
+long int Process::UpTime() { 
+  return LinuxParser::UpTime() - LinuxParser::UpTime(pid_); 
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+//bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+//bool Process::operator<(Process const& a) const { return stol(Ram()) < stol(a.Ram()); }
+//bool Process::operator<(Process const& a) const { return pid_ < a.pid_; }
+//bool Process::operator<(Process & a) { CpuUtilization()  < a.CpuUtilization(); }
+//bool Process::operator<(Process & a) { pid_  < a.pid_; }
+  bool Process::operator<(Process & a) { cpuUtil_ < a.cpuUtil_;}
